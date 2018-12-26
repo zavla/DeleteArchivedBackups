39f6b4e59c3ba0f16726c6192742b112a2064716
zakhar.malinovskiy <zakhar.malinovskiy@gmail.com>
2018-12-26 17:22:07 +0200

    1st commit

---
+++ DeleteArchivedBackups.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"dblist"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"syscall"
+)
+
+func main() {
+
+	// Example of config file:
+	// [{"path":"g:/ShebB", "Filename":"buh_log8", "Days":1},
+	// {"path":"g:/ShebB", "Filename":"buh_log3", "Days":1},
+	// {"path":"g:/ShebB", "Filename":"buh_prom8", "Days":1},
+	// ]
+	
+
+	delArchived := flag.Bool("delete_archived", false, "deletes files with attribute archived set")
+	configfile := flag.String("config", "", "full config file name (lists databases files groups)")
+	flag.Parse()
+	if *configfile == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	conf := dblist.ReadConfig(*configfile)
+
+	unique := dblist.GetUniquePaths(conf)
+
+	files := dblist.ReadFilesFromPaths(unique)
+
+	lastfilesmap := make(map[string][]dblist.FileInfoWin)
+
+	if *delArchived {
+		for dir, slice := range files {
+			lastfilesslice := dblist.GetLastFilesGroupedByFunc(slice, dblist.GroupFunc)
+			// lastfilesslice is descending
+			lastfilesmap[dir] = lastfilesslice
+		}
+		deleteArchivedFiles(files, lastfilesmap)
+	} else {
+		flag.Usage()
+	}
+
+}
+
+func deleteArchivedFiles(files, exceptfiles map[string][]dblist.FileInfoWin) {
+	for dir, slice := range files { // for every dir
+		for _, finf := range slice { // for every file in dir
+			if !finf.IsDir() { // dont touch subdirs and not archived
+				if (finf.WinAttr & syscall.FILE_ATTRIBUTE_ARCHIVE) != 0 {
+
+					// search if file in question is in the exception list
+					curmap := exceptfiles[dir]                         // exceptfiles is descending
+					pos := sort.Search(len(curmap), func(i int) bool { // bin search
+						return curmap[i].Name() <= finf.Name() // <= for the descending slice
+
+					})
+					if pos < len(curmap) && curmap[pos].Name() == finf.Name() {
+						// found
+						continue // file should not be deleted. this is the last file in the group
+					}
+					fullFilename := filepath.Join(dir, finf.Name())
+					log.Printf("deleting file %s\n", fullFilename)
+					if true { //DEBUG
+						err := os.Remove(fullFilename)
+						if err != nil {
+							log.Printf("%s\n", err)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+//
